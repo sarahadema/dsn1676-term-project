8bd69ad1522432ea76827ca6990e4cfdbbe1aa37
sarahadema <SarahAdema@Sarahs-MacBook-Pro.local>
2015-03-14 16:17:00 -0400

    Applied more JS
    added java script and divs to notebook and stickers to make the
    additional information show and hide like they are supposed to.
    Styled the more button

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ var $infoTshirt = $('.info-tshirt');
 var $btnStickers = $('.btn-stickers');
 var $infoStickers = $('.info-stickers');
 
+var $btnNotebook = $('.btn-notebook');
+var $infoNotebook = $('.info-notebook');
+
 $btnTshirt.on('click', function(){
 	$infoTshirt.toggleClass('js-btn-tshirt');
 })
 
 $btnStickers.on('click',function(){
 	$infoStickers.toggleClass('js-btn-stickers');
+})
+
+$btnNotebook.on('click',function(){
+	$infoNotebook.toggleClass('js-btn-notebook');
 })
